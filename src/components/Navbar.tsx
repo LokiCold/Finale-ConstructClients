e50ef9179a62a512b192aca981c1bcdf565e6975
LokiCold <nathanealseg@gmail.com>
2025-03-20 16:55:43 +0000

    Make second logo visible on mobile devices with responsive sizing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,24 +19,24 @@ const Navbar = ({ scrollToVideo, scrollToBooking, scrollToTestimonials, scrollTo
 
   return (
     <nav className="fixed w-full z-[100] bg-[#38071e] shadow-lg">
-      <div className="container mx-auto px-4 relative">
+      <div className="container mx-auto px-1 sm:px-4 relative">
         <div className="flex justify-between items-center h-20">
           {/* Logo Container */}
-          <div className="flex items-center gap-12">
+          <div className="flex items-center gap-3 sm:gap-6 md:gap-12">
             {/* Main Logo */}
-            <div className="h-[50px] flex items-center">
+            <div className="h-[40px] sm:h-[50px] flex items-center">
               <img 
                 src="/Untitled design (5).svg" 
                 alt="ConstructClients.ai Logo" 
-                className="h-full w-auto object-contain scale-[1.5]"
+                className="h-full w-auto object-contain scale-[1.2] sm:scale-[1.5]"
               />
             </div>
             {/* Second Logo */}
-            <div className="h-[140px] hidden md:flex items-center -my-8">
+            <div className="h-[60px] sm:h-[100px] md:h-[140px] flex items-center -my-2 sm:-my-4 md:-my-8">
               <img 
                 src="/2-LOGO.png" 
                 alt="Name Logo" 
-                className="h-full w-auto object-contain scale-[3.2] brightness-150 contrast-125 saturate-125"
+                className="h-full w-auto object-contain scale-[1.8] sm:scale-[2.5] md:scale-[3.2] brightness-150 contrast-125 saturate-125"
               />
             </div>
           </div>
